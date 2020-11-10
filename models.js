ba467706da5636a83952826ffb3ba606b662369b
aditiyadwiramadani88 <aditiyadwiramadani88@gamil.com>
2020-11-10 05:46:35 -0600

    models Databases

--- models.js
+++ models.js
@@ -1,8 +1,4 @@
 const { Sequelize, DataTypes } = require('sequelize')
-
-
-
-
 const sequelize = new Sequelize('nodeapi', 'root', '', {
     host: 'localhost',
     dialect: 'mysql'
@@ -19,13 +15,18 @@ const Product = sequelize.define('Product', {
 });
 
 
+
+const User = sequelize.define('User', {
+    username:{
+        type: DataTypes.STRING,
+        unique: true
+    },
+    password: {
+        type: DataTypes.STRING
+    }
+})
+
 const data1 = []
-
-
-
-    
-
-
-
 exports.db = sequelize
 exports.product = Product
+exports.user = User
